2f1ff4e3321fbcb8a1ea15502387a57c9b411806
R0seP <sophie.hecht28@gmail.com>
2024-11-01 16:09:24 +0100

    re-upload

--- class_correction_without_Senecio.R
+++ class_correction_without_Senecio.R
@@ -2,7 +2,7 @@
 #and modifies it so that the garbage category "Clumps" is no longer counted in 
 #the number and percentage of pollen grains in a sample. 
 #original data with adjusted classifications
-adj <- read.csv("pollen_adj_class.csv", h = T)
+adj <- read.csv("pollen_adj_class_without_Senecio.csv", h = T)
 
 #create new data frame 
 adj_new <- adj
@@ -13,25 +13,25 @@ adj_new$nPoll <- adj$nPoll - (adj$nPoll * adj$P_zzClumps)
 adj_new$nGarb <- adj$nGarb + (adj$nPoll * adj$P_zzClumps)
 
 #add column to old data frame that sums up proportions of all pollen (but not the clumps)
-adj$P_pollen <- rowSums(adj[, 8:42], na.rm = TRUE)
+adj$P_pollen <- rowSums(adj[, 8:41], na.rm = TRUE)
 
 #adjust the percentages of pollen grains so that they add up to 100% again
 for (r in 1:length(adj_new$Site)){
-  for (c in 1:36){
+  for (c in 1:35){
     adj_new[r,7+c] <- adj[r,7+c]/adj[r,"P_pollen"]
   }
 }
 
 #check if loop worked and all row sums in new data frame add up to 1 without clumps
-rowSums(adj_new[, 8:42], na.rm = TRUE)
+rowSums(adj_new[, 8:41], na.rm = TRUE)
   #add to 1 but some 0's there because of no pollen data
   #okay because I want to keep original data, but note which ones:
 which(rowSums(adj_new[, 8:42], na.rm = TRUE) == 0)
-#row 216, row 296 and row 468 contain NA's because of no pollen recognized in the sample
+#row 216, row 296, row 352 and row 468 contain NA's because of no pollen recognized in the sample
 
 #delete clumps percentages from new data frame
-adj_new <- adj_new[,-43]
+adj_new <- adj_new[,-42]
 
 
 #save new data frame
-write.csv(adj_new, "adj_new.csv", row.names = F)
+write.csv(adj_new, "adj_new_without_Senecio.csv", row.names = F)
